6a1c6b7b8cdef5d48171402f4576b514e7a69270
W. Glen Boyd <glen@wgbdev.com>
2024-01-07 17:16:33 -0500

    fixed script.

--- curl-it.sh
+++ curl-it.sh
@@ -1,1 +1,1 @@
-curl -fsSL "https://raw.githubusercontent.com/wgbdev/init-scripts/main/build-util.sh" > build-util.sh && chomd +x build-util.sh && ./build-util.sh
+curl -fsSL "https://raw.githubusercontent.com/wgbdev/init-scripts/main/build-util.sh" > build-util.sh && chmod +x build-util.sh && ./build-util.sh
