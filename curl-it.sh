6aeac03484eef7171b34825c2546642dadad7526
W. Glen Boyd <glen@wgbdev.com>
2024-01-11 09:35:54 -0500

    Updated

--- curl-it.sh
+++ curl-it.sh
@@ -1,1 +1,1 @@
-curl -fsSL "https://raw.githubusercontent.com/wgbdev/init-scripts/main/build-util.sh" > build-util.sh && chmod +x build-util.sh && ./build-util.sh
+curl -fsSL "https://raw.githubusercontent.com/wgbdev/init-scripts/main/bldutl.sh" > bldutl.sh && chmod +x bldutl.sh && ./bldutl.sh
